7673bef25215da5ef38d6f429b5fad1ff4b6b29b
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-16 07:40:05 +0200

    16 x 16 grid (256 elements), CSS is likely not a final solution

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,6 @@
 console.log("Hello world");
 const sketch_area = document.querySelector("#sketch");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 256; i++) {
     const sketch_pixel = document.createElement("div");
     sketch_pixel.textContent = "X";
     sketch_pixel.className = "pixel";
