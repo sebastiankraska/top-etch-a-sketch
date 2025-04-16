19114cbb4ef6a3d1d34c3f37aca51a5ad0473357
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-16 07:26:54 +0200

    Make JS create 100 divs (content: 'X' for visibility)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+const sketch_area = document.querySelector("#sketch");
+for (let i = 0; i < 100; i++) {
+    const sketch_pixel = document.createElement("div");
+    sketch_pixel.textContent = "X";
+    sketch_area.appendChild(sketch_pixel);    
+}
