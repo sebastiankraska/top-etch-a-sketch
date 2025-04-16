5e34a3993880115637bf18e6ad4dfdf5efbe25f8
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-16 07:33:29 +0200

    10 x 10 grid with flexbox (without CSS grid)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,5 +3,6 @@ const sketch_area = document.querySelector("#sketch");
 for (let i = 0; i < 100; i++) {
     const sketch_pixel = document.createElement("div");
     sketch_pixel.textContent = "X";
+    sketch_pixel.className = "pixel";
     sketch_area.appendChild(sketch_pixel);    
 }
