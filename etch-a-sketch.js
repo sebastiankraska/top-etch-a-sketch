386aa4fec59b3a5f68a2eed11caf5bc6667ac6f2
Sebastian Kraska <140939975+sebastiankraska@users.noreply.github.com>
2025-04-17 07:37:24 +0200

    Major change: Size can be changed dynamically (add eventlistener, etc.)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,20 +7,37 @@ console.log("Hello world");
 
 // create pixel grid
 function createPixelGrid(input_size) {
+
+    // deleting old pixels
+    const allPixels = document.querySelectorAll(".pixel");
+    // allPixels.remove(); // THIS DOES NOT WORK!
+    allPixels.forEach(pixel => pixel.remove()); // This does work :)
+
+    // select area and calculate amount of total new pixels and size of each pixel
     const sketch_area = document.querySelector("#sketch");
     const output_size = input_size ** 2;
+    const pixel_size = 960 / input_size; // container div is 960px wide
+    
+    // select input elements
+    const input = document.querySelector("input");
+    const button = document.querySelector("button");
+
+    // debug:
     console.log(output_size);
-    const pixel_size = 960 / input_size;
+    
+    // Add pixels to container
     for (let i = 0; i < output_size; i++) {
         const sketch_pixel = document.createElement("div");
         sketch_pixel.textContent = "X";
         sketch_pixel.className = "pixel";
         sketch_pixel.style.width = pixel_size + "px"; // TODO: is concatenating a "px" ugly?
-        sketch_area.appendChild(sketch_pixel);  
-        // TODOs:
-        // Add event listener to input/button
+        sketch_area.appendChild(sketch_pixel);
     }
+    button.addEventListener('click', (event) => {
+        const inputValue = input.value;
+        createPixelGrid(inputValue);
+    })
 };
 
 // create initial pixel grid
-createPixelGrid(30); 
+createPixelGrid(20); 
